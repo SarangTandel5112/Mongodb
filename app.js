94b8631614ab5e1a540f70a1d4d0a4c86f8d0ed9
Charmi Rathod <rathodcharmi12@gmail.com>
2022-04-07 14:32:58 +0530

    new files

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 import express from 'express'
 import connectDB from '../demo/db/connectDB.js';
+import join from 'path';
 
 const app = express()
 const port = process.env.PORT || '3000'
 const DATABASE_URL = process.env.DATABASE_URL || "mongodb://localhost:27017/schooldb"
 
 connectDB(DATABASE_URL)
+
+//static files
+app.use(express.static(join(process.cwd, "public")))
 
 app.listen(port, () => {
     console.log(`server listening at http://localhost:${port}`);
---
+++ controllers/studentController.js
@@ -0,0 +1,5 @@
+class StudentController{
+    static getAllDoc = () => {
+        res.render()
+    }
+}
